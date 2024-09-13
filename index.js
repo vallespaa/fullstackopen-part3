30362c3ffe4684dabc9e3fd3cbaedfef9c91fde2
Diego <diva.blas@gmail.com>
2024-09-13 16:33:58 +0200

    3.13

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/person')
 
 app.use(morgan('tiny'));
 
@@ -63,7 +65,9 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
